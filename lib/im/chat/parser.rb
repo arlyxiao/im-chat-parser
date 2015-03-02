b3a3c8ea7ccee564468b9ada6da043ba51773645
arlyxiao <kingla_pei@163.com>
2015-03-02 15:15:42 +0800

    first commit

---
+++ lib/im/chat/parser.rb
@@ -0,0 +1,9 @@
+require "im/chat/parser/version"
+
+module Im
+  module Chat
+    module Parser
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/im/chat/parser/version.rb
@@ -0,0 +1,7 @@
+module Im
+  module Chat
+    module Parser
+      VERSION = "0.0.1"
+    end
+  end
+end
